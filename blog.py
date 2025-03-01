50be679fce632d90b0cabe5743802ee50dd7aaf1
Rahul Joshi <rahul2210085@akgec.ac.in>
2025-03-02 00:53:48 +0530

    done for both the apps

--- blog.py
+++ blog.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+
 import os
 import requests
 import random
@@ -7,19 +6,47 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 from dotenv import load_dotenv
 import cohere
+import datetime
 load_dotenv()
 
-# Initialize Firebase
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
-firebase_admin.initialize_app(cred, {'storageBucket': f"{os.getenv('FIREBASE_PROJECT_ID')}.firebasestorage.app"})
-db = firestore.client()
-bucket = storage.bucket()
+
+#Initialize Firebase
+
+def initialize_firebase_app(credential_path, app_name, project_id):
+    cred = credentials.Certificate(credential_path)
+    return firebase_admin.initialize_app(
+        cred,
+        {'storageBucket': f"{project_id}.firebasestorage.app"},
+        name =  app_name
+    )
+
+#hotel soni
+app1 = initialize_firebase_app(
+    os.getenv("FIREBASE_CREDENTIALS_PATH_SONI"),
+    "project1",
+    os.getenv("FIREBASE_PROJECT_ID_SONI")
+)
+
+#hotel jbm
+app2 = initialize_firebase_app(
+    os.getenv("FIREBASE_CREDENTIALS_PATH_jbm"),
+    "project2",
+    os.getenv("FIREBASE_PROJECT_ID_jbm")
+)
+
 
 # Initialize Cohere
 cohere_api_key = os.getenv("COHERE_API_KEY")
 cohere_client = cohere.Client(cohere_api_key)
 
-def get_random_input():
+def get_firestore_client(app):
+    return firestore.client(app=app)
+
+def get_storage_bucket(app):
+    return storage.bucket(app=app)
+#get random keyword
+def get_random_input(app):
+    db = get_firestore_client(app)
     keywords_ref = db.collection('keyword').stream()
     keyword_list = [doc.to_dict().get("phrase") for doc in keywords_ref]
     
@@ -27,6 +54,8 @@ def get_random_input():
         return None
     return random.choice(keyword_list)
 
+
+#genrate blog
 def generate_blog_content(keyword):
     try:
         response = cohere_client.generate(
@@ -75,7 +104,8 @@ def fetch_image(keyword):
     data = response.json()
     return [photo['urls']['regular'] for photo in data['results']] 
 
-def upload_image(image_url, keyword):
+def upload_image(app,image_url, keyword):
+    bucket = get_storage_bucket(app)
     response = requests.get(image_url, stream=True)
     if response.status_code != 200:
         return None
@@ -85,9 +115,13 @@ def upload_image(image_url, keyword):
     blob.make_public()
     return blob.public_url
 
+def save_blog(app,blog_data):
+    db = get_firestore_client(app)
+    db.collection('blog').add(blog_data)
 
-def main():
-    keyword = get_random_input()
+
+def main(app):
+    keyword = get_random_input(app)
     if not keyword:
         keyword = "trip to banaras uttar pradesh India" 
 
@@ -96,17 +130,18 @@ def main():
     if not title or not content:
         return None
     image_urls = fetch_image(keyword)
-    image_url = upload_image(image_urls[0], keyword)
+    image_url = upload_image(app,image_urls[0], keyword)
 
     blog_data = {
         "title": title,
         "description": content,
         "image": image_url,
-        "createdAt " : firestore.SERVER_TIMESTAMP
+        "createdAt " : datetime.datetime.now()
     }
 
-    db.collection('blog').add(blog_data)
+    save_blog(app, blog_data)
 
 
 if __name__ == "__main__":
-    main()
+    main(app1)
+    main(app2)
